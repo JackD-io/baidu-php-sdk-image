45f886d1bc573519b9e3af5b3cd8636c3fa1f6cc
Jack <dongkaipo@126.com>
2018-10-09 14:15:49 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+# ReadMe
+
+## Install
+
+```
+composer reuqire dongkaipo/baidu-php-sdk-image
+```
+
+## Usage
+
+```
+<?php
+
+```
+
+## Baidu Api Document
+
+* [Baidu Image Document](https://cloud.baidu.com/doc/IMAGERECOGNITION/ImageClassify-PHP-SDK.html)
--- src/Dongkaipo/ImageClassify.php
+++ src/Dongkaipo/ImageClassify.php
@@ -1,12 +1,21 @@
 <?php
+
 namespace Dongkaipo\Baidu;
+
+use Image\AipImageClassify;
 
 class ImageClassify
 {
-    public function __construct()
+    private $client;
+
+    public function __construct($appId, $apiKey, $apiSecretKey)
     {
+        $this->client = new AipImageClassify($appId, $apiKey, $apiSecretKey);
+    }
 
-
+    public function getClient()
+    {
+        return $this->client;
     }
 
 }
